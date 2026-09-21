c44627e19fb604b7d9b08e0e2b3d2332db42aef0
janghw0126 <janghw232625@gmail.com>
2026-09-21 15:54:41 +0900

    feat(frontend): 계정 생성일을 한국 날짜로 변환하는 유틸 추가

--- frontend/src/entities/admin-account/index.ts
+++ frontend/src/entities/admin-account/index.ts
@@ -1,3 +1,4 @@
 export { getAdminAccounts } from "./api/getAdminAccounts";
+export { formatAccountDate } from "./lib/formatAccountDate";
 export type { AdminAccount, AdminAccountList } from "./model/types";
 export { adminAccountsQueryKey, useAdminAccountsQuery } from "./model/useAdminAccountsQuery";
