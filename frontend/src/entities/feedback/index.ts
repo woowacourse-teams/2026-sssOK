8d66a36b0aac4a3d8b3af4872c5bfa948df60254
janghw0126 <janghw232625@gmail.com>
2026-09-21 14:00:37 +0900

    feat(frontend): 의견 단건 조회 API 와 조회 훅 추가

--- frontend/src/entities/feedback/index.ts
+++ frontend/src/entities/feedback/index.ts
@@ -1,7 +1,9 @@
+export { getAdminFeedback } from "./api/getAdminFeedback";
 export { getAdminFeedbacks } from "./api/getAdminFeedbacks";
 export { describeUserAgent } from "./lib/describeUserAgent";
 export { formatFeedbackDateTime, formatFeedbackTime } from "./lib/formatFeedbackTime";
 export { collectFrontendVersions } from "./lib/frontendVersions";
-export type { AdminFeedback, AdminFeedbackPage } from "./model/types";
+export type { AdminFeedback, AdminFeedbackDetail, AdminFeedbackPage } from "./model/types";
+export { adminFeedbackQueryKey, useAdminFeedbackQuery } from "./model/useAdminFeedbackQuery";
 export { adminFeedbacksQueryKey, useAdminFeedbacksQuery } from "./model/useAdminFeedbacksQuery";
 export { FeedbackItem } from "./ui/FeedbackItem";
